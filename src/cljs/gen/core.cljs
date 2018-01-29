7693457f958c61db11165e8200d036259fbcbf09
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2018-01-29 20:17:27 +0100

    Attempt to understand quil

--- src/cljs/gen/core.cljs
+++ src/cljs/gen/core.cljs
@@ -1,8 +1,10 @@
 (ns gen.core
-  (:require [clojure.spec.alpha :as s]
+  (:require [clojure.string :as string]
+            [clojure.spec.alpha :as s]
             [clojure.test.check.generators :as gen]
-            [quil.core :as q]))
-
+            [cljs.core.async :as async :include-macros true]
+            [quil.core :as q]
+            [quil.middleware :as m]))
 
 (defn log [& args]
   (apply (.-log js/console) args))
@@ -26,16 +28,6 @@
                        :group ::group)))
 
 
-(defn init
-  "Called on page load."
-  []
-  (log (s/valid? ::rule (seq "F"))
-       (s/valid? ::rule (seq "-F"))
-       (s/valid? ::rule (seq "[F]"))
-       (s/valid? ::rule (seq "F[-F]"))
-       (s/valid? ::rule (seq "F[-F[+FF]]"))))
-
-
 (defn create-rule
   "Create a rule of `size` using test.check."
   [size]
@@ -50,15 +42,29 @@
 
 
 (def rules (repeatedly #(create-rule 4)))
-(log "Some generated rules:" (take 3 rules))
+(comment (log "Some generated rules:" (take 3 rules)))
 ;; => ("FFF[[-]]" "-F[F-+]" "F[F[[+][-+-][[++F]--FF]]++]+-")
 
 
-(log "Parsed F[-F]:" (parse-rule "F[-F]"))
+(comment (log "Parsed F[-F]:" (parse-rule "F[-F]")))
 ;; => [[:char "F"]
 ;;     [:group {:push "["
 ;;              :children [[:op "-"] [:char "F"]]
 ;;              :pop "]"}]]
+
+
+(defn tree
+  "Create a string representing the tree to draw."
+  ([axiom rule] (tree axiom rule 2 axiom))
+  ([axiom rule steps acc]
+   (if (= 0 steps)
+     acc
+     (let [new-acc (string/replace acc axiom rule)]
+       (recur axiom rule (dec steps) new-acc)))))
+
+
+(def tree-chan (async/to-chan "F-[FF]FF"))
+
 
 (defn create-element [type]
   (.createElement js/document type))
@@ -68,38 +74,62 @@
   (.appendChild parent el))
 
 
-(def body (.-body js/document))
-
-
-(when-let [old-sketch (.querySelector js/document "#single-sketch")]
-  (.removeChild body old-sketch))
-
-
-(def canvas (create-element "canvas"))
-(set! (.-id canvas) "single-sketch")
-(append body canvas)
+(defn prepend [parent el]
+  (.insertBefore parent el (.-firstChild parent)))
 
 
 (defn setup
   []
-  (q/frame-rate 4)
-  (q/background 200))
+  (q/frame-rate 10)
+  (q/background 200)
+  {:x 0
+   :y 0
+   :first true})
 
+(defn update-fn
+  [s]
+  (-> s
+    (assoc :first false)
+    (assoc :op (async/poll! tree-chan))))
 
 (defn draw
+  [s]
+  (q/translate 100 200)
+  (q/rotate Math/PI)
+  (when-let [op (:op s)]
+    (log op)
+    (condp = op
+      "F" (do
+            (q/line 0 0 0 20)
+            (q/translate 0 20))
+      "-" (do
+            (q/rotate (- Math/PI 10)))
+      "[" (q/push-matrix)
+      "]" (q/pop-matrix)
+      nil)))
+
+(defn render []
+
+  (def body (.-body js/document))
+
+  (when-let [canvas (.querySelector js/document "canvas")]
+    (.removeChild body canvas))
+
+  (def canvas (create-element "canvas"))
+  (set! (.-id canvas) "w00t")
+  (prepend body canvas)
+
+  (q/sketch
+    :host "w00t"
+    :size [200 200]
+    :setup setup
+    :draw draw
+    :update update-fn
+    :middleware [m/fun-mode])
+  )
+
+(defn init
+  "Called on page load."
   []
-  (q/stroke (q/random 255))
-  (q/stroke-weight (q/random 10))
-  (q/fill (q/random 255))
+  (.setTimeout js/window #(render) 1000))
 
-  (let [diam (q/random 100)
-        x    (q/random (q/width))
-        y    (q/random (q/height))]
-    (q/ellipse x y diam diam)))
-
-
-(q/sketch
-  :host canvas
-  :size [200 200]
-  :setup setup
-  :draw draw)
