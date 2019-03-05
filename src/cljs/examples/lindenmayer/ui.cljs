d49de7cc3ef729add8ca96f63c43573e0264ce54
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2019-03-05 21:17:35 +0100

    Make both update and draw reloadable

--- src/cljs/examples/lindenmayer/ui.cljs
+++ src/cljs/examples/lindenmayer/ui.cljs
@@ -7,7 +7,7 @@
             [cljs.core.async :as async :include-macros true]))
 
 
-(defn draw [{:keys [w h op n first]}]
+(defn s-draw [{:keys [w h op n first]}]
   (q/pop-matrix)
   (when first
     (q/translate (/ w 2) (/ h 1.2))
@@ -16,8 +16,8 @@
     (condp = op
       "F" (do
             (q/stroke-weight (q/random (* 5 n)))
-            (q/stroke (q/random 200 250)
-                      (q/random 50 200)
+            (q/stroke (q/random 0 50)
+                      (q/random 150 200)
                       (q/random 0 50))
             (let [l (q/random 1 (* 20 n))]
               (q/line 0 0 0 l)
@@ -30,6 +30,16 @@
     (q/push-matrix)))
 
 
+(defn s-update [{:keys [chan n] :as s}]
+  (let [op (async/poll! chan)]
+    (-> s
+      (assoc :first false)
+      (assoc :op op)
+      (assoc :n (condp = op
+                  "[" (* n 0.8)
+                  "]" (/ n 0.8)
+                  n)))))
+
 (defn turtle
   "Pull operations from chan and draw a lindenmayer tree."
   [canvas w h chan]
@@ -40,17 +50,13 @@
     :setup (fn []
              (q/frame-rate 1000)
              (q/background 255)
-             {:first true :n 1 :w w :h h})
-    :update (fn [{:keys [n] :as s}]
-              (let [op (async/poll! chan)]
-                (-> s
-                  (assoc :first false)
-                  (assoc :op op)
-                  (assoc :n (condp = op
-                              "[" (* n 0.8)
-                              "]" (/ n 0.8)
-                              n)))))
-    :draw #'draw))
+             {:first true
+              :n     1
+              :w     w
+              :h     h
+              :chan  chan})
+    :update #'s-update
+    :draw #'s-draw))
 
 
 (defn init
